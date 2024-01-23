b50dd3c2a7d0bb6e41e712f8b26327aa4eaa79a1
noah.ruan <noahruan@gmail.com>
2024-01-23 15:28:58 +0800

    chore: update

---
+++ assets/preview-d7f6b102.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-8c20aa1f.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-c2940fce.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-c2940fce.js","./iframe-8c20aa1f.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./_getPrototype-d883cfb7.js","./index-8ce4a492.js","./index-d475d2ea.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-d7f6b102.js.map
